paetzke/py-autopep8.el

commit 4e1848a7932a6cb522f73521baea9bb4fd8bf565

Replace decf and incf
These 2 functions are not built into Emacs.

py-autopep8.el
@@ -67,7 +67,7 @@ Note that `--in-place' is used by default."
                 (forward-line len)
                 (let ((text (buffer-substring start (point))))
                   (with-current-buffer target-buffer
-                    (decf line-offset len)
+                    (setq line-offset (+ line-offset len))
                     (goto-char (point-min))
                     (forward-line (- from len line-offset))
                     (insert text)))))
@@ -75,7 +75,7 @@ Note that `--in-place' is used by default."
               (with-current-buffer target-buffer
                 (goto-char (point-min))
                 (forward-line (- from line-offset 1))
-                (incf line-offset len)
+                (setq line-offset (+ line-offset len))
                 (kill-whole-line len)))
              (t
               (error "invalid rcs patch or internal error in py-autopep8-apply-rcs-patch")))))))))

commit 2f0f5634af8c0f01c7ae7eede30610a7201afe2c

Fixed typo
This fixes a typo introduced in 4e1848a7932a6cb522f73521baea9bb4fd8bf565

py-autopep8.el
@@ -67,7 +67,7 @@ Note that `--in-place' is used by default."
                 (forward-line len)
                 (let ((text (buffer-substring start (point))))
                   (with-current-buffer target-buffer
-                    (setq line-offset (+ line-offset len))
+                    (setq line-offset (- line-offset len))
                     (goto-char (point-min))
                     (forward-line (- from len line-offset))
                     (insert text)))))